145cc0f883683be9007612927b0e9319f4061065
sohyun <ksh96611@naver.com>
2025-03-03 16:37:30 +0900

    TDD

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -40,11 +40,9 @@ public class CafeKiosk {
     }
 
     public int calculateTotalPrice() {
-        int totalPrice = 0;
-        for (Beverage beverage : beverages) {
-            totalPrice += beverage.getPrice();
-        }
-        return totalPrice;
+        return beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
     }
 
     public Order createOrder(){
@@ -68,4 +66,5 @@ public class CafeKiosk {
 
         return new Order(currentDateTime, beverages);
     }
+
 }
